c264503b4617772c5e14a50931263c6c84aa81fa
mati <matiasvaldez8184@gmail.com>
2022-09-19 00:00:50 -0300

    basic styling

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 
-function LloraMateo() {
-    const [count, setCount] = useState(0);
+
+function Album({album}) {
+    const { title, imagePath } = album;
     return (
-        <button onClick={() => setCount(count => count + 1)}>
-            Mateo lloro {count}
-        </button>
+        <div className='h-[100vh]'>
+            <div className='flex justify-center'>
+                <div className='flex-col'>
+                    <img className='w-3/12 h-4/6 aspect-[6/7]' src={imagePath} alt={title} />
+                    <h2 className='w-fit'>{title}</h2>
+                </div>
+            </div>
+        </div>
     )
 }
-export default LloraMateo
+export default Album
---
+++ utils/data/mock.ts
@@ -0,0 +1,52 @@
+export const data = [{
+    title: "The Piper At The Gates Of Dawn",
+    imagePath: "https://i.ibb.co/YNSdZm8/the-piper-at-the-gates-of-dawn.jpg"
+}, 
+{
+    title: "A Saucerful Of Secrets",
+    imagePath: "https://i.ibb.co/pwB0jdr/saucerful-of-secrets.jpg"
+}, 
+{
+    title: "Ummagumma",
+    imagePath: "https://i.ibb.co/6gkD1G2/ummagumma.jpg"
+}, 
+{
+    title: "Atom Heart Mother",
+    imagePath: "https://i.ibb.co/vJQXdRV/atom-heart.jpg"
+}, 
+{
+    title: "Meddle",
+    imagePath: "https://i.ibb.co/2P0x3fF/meddle.png"
+}, 
+{
+    title: "The Dark Side Of The Moon",
+    imagePath: "https://i.ibb.co/7YWXHdp/dark-side-of-the-moon.png"
+}, 
+{
+    title: "Wish You Were Here",
+    imagePath: "https://i.ibb.co/3vqZz0F/wish-you-were-here.jpg"
+}, 
+{
+    title: "Animals",
+    imagePath: "https://i.ibb.co/xLRNNxp/animals.jpg"
+}, 
+{
+    title: "The Wall",
+    imagePath: "https://i.ibb.co/GCSbf8n/the-wall.png"
+}, 
+{
+    title: "The Final Cut",
+    imagePath: "https://i.ibb.co/0FFsgN2/the-final-cut.png"
+}, 
+{
+    title: "A Momentary Lapse Of Reason",
+    imagePath: "https://i.ibb.co/hD2BXcJ/momentary-lapse-of-reason.jpg"
+}, 
+{
+    title: "The Division Bell",
+    imagePath: "https://i.ibb.co/zhJwb7h/the-division-bell.jpg"
+},
+{
+    title: "The Endless River",
+    imagePath: "https://i.ibb.co/DLBS3cH/the-endless-river.jpg"
+}]
